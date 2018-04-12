d993a59eb0aa25c604f9eb0e18e1371aeba0d76c
Alexander Naumov <me@alexandernaumov.de>
2018-04-12 15:20:38 +0200

    add reminder module label

--- languages/de/modules.php
+++ languages/de/modules.php
@@ -1,3 +1,4 @@
 <?php
 
 $GLOBALS['TL_LANG']['MOD']['catalog-manager-mailer'] = [ 'Mailer', 'Catalog Manager Mailer Tool.' ];
+$GLOBALS['TL_LANG']['MOD']['catalog-manager-reminder'] = [ 'Reminder', 'Catalog Manager Reminder Tool.' ];
