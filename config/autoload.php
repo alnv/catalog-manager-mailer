af752707e8ec3f0294cf99870f1dc49be13cd098
Alexander Naumov <me@alexandernaumov.de>
2018-04-17 14:37:38 +0200

    add missing class

--- config/autoload.php
+++ config/autoload.php
@@ -9,6 +9,7 @@ ClassLoader::addClasses([
     'CatalogManager\Mailer\tl_mailer' => 'system/modules/catalog-manager-mailer/classes/tl_mailer.php',
     'CatalogManager\Mailer\tl_module' => 'system/modules/catalog-manager-mailer/classes/tl_module.php',
     'CatalogManager\Mailer\tl_reminder' => 'system/modules/catalog-manager-mailer/classes/tl_reminder.php',
+    'CatalogManager\Mailer\AttachmentBuilder' => 'system/modules/catalog-manager-mailer/AttachmentBuilder.php',
     'CatalogManager\Mailer\FrontendEditingTrigger' => 'system/modules/catalog-manager-mailer/FrontendEditingTrigger.php'
 ]);
 
