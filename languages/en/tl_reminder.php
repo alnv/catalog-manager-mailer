00a52a9a3b1ad3fbed23033d5e49f66461a1a9cf
Alexander Naumov <me@alexandernaumov.de>
2018-04-17 14:36:59 +0200

    add missing labels

--- languages/en/tl_reminder.php
+++ languages/en/tl_reminder.php
@@ -1,3 +1,38 @@
 <?php
 
-//
+$GLOBALS['TL_LANG']['tl_reminder']['new'] = [ 'Create reminder', 'Here you can create a reminder.' ];
+
+$GLOBALS['TL_LANG']['tl_reminder']['reminder_settings'] = 'Reminder settings';
+$GLOBALS['TL_LANG']['tl_reminder']['attachment_settings'] = 'Attachment settings';
+$GLOBALS['TL_LANG']['tl_reminder']['general_settings'] = 'General settings';
+
+$GLOBALS['TL_LANG']['tl_reminder']['name'] = [ 'Name', 'Please enter a name.' ];
+$GLOBALS['TL_LANG']['tl_reminder']['dbTable'] = [ 'Table', 'Please select a name.' ];
+$GLOBALS['TL_LANG']['tl_reminder']['mailer_id'] = [ 'Mailer', 'Please select a mailer.' ];
+$GLOBALS['TL_LANG']['tl_reminder']['attachment_template'] = [ 'Template', 'Please select a template.' ];
+$GLOBALS['TL_LANG']['tl_reminder']['dbTaxonomy'] = [ 'Filtering', 'Here you can filter you reminder view.' ];
+$GLOBALS['TL_LANG']['tl_reminder']['use_attachment'] = [ 'Create attachment', 'Here you can create a attachment.' ];
+$GLOBALS['TL_LANG']['tl_reminder']['tableColumns'] = [ 'Table Columns', 'Please select columns to be displayed in the table.' ];
+
+$GLOBALS['TL_LANG']['tl_reminder']['first_execution'] = [ 'First Execution', 'Here you can specify the date of the first execution.'];
+$GLOBALS['TL_LANG']['tl_reminder']['last_execution'] = [ 'Last Execution', 'Here you will find the date of the last execution.' ];
+$GLOBALS['TL_LANG']['tl_reminder']['execution_time'] = [ 'Execution time', 'Please enter an execution time.' ];
+$GLOBALS['TL_LANG']['tl_reminder']['dbOrderBy'] = [ 'Sorting', 'Here you can influence the sort order.' ];
+$GLOBALS['TL_LANG']['tl_reminder']['interval'] = [ 'Interval', 'Please select an interval.' ];
+
+$GLOBALS['TL_LANG']['tl_reminder']['show'] = [ 'Show reminder', 'Here you can show the reminder with the ID "% s".' ];
+$GLOBALS['TL_LANG']['tl_reminder']['edit'] = [ 'Edit reminder', 'Here you can edit the reminder with the ID "% s".' ];
+$GLOBALS['TL_LANG']['tl_reminder']['copy'] = [ 'Copy reminder', 'Here you can copy the reminder with the ID "% s".' ];
+$GLOBALS['TL_LANG']['tl_reminder']['send'] = [ 'Start reminder', 'Here you can start the reminder with the ID "% s".' ];
+$GLOBALS['TL_LANG']['tl_reminder']['delete'] = [ 'Delete reminder', 'Here you can delete the reminder with the ID "% s".' ];
+
+$GLOBALS['TL_LANG']['tl_reminder']['intervalMessages'] = [
+
+    'once' => 'Once',
+    'daily' => 'Daily',
+    'weekly' => 'Weekly',
+    'semimonthly' => 'Semimonthly',
+    'monthly' => 'Monthly',
+    'quarter' => 'Quarter',
+    'yearly' => 'Yearly'
+];
