cb4d998b9fc04e26c3123e500cfb3459fc2022c8
Alexander Naumov <alexandernaumov@MacBookPro.fritz.box>
2025-03-08 21:52:10 +0100

    rm code

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -24,20 +24,3 @@ $GLOBALS['BE_MOD']['catalog-manager-extensions']['cm-reminder'] = [
 $GLOBALS['TL_HOOKS']['catalogManagerEntityOnCreate'][] = [FrontendEditingTrigger::class, 'onCreate'];
 $GLOBALS['TL_HOOKS']['catalogManagerEntityOnUpdate'][] = [FrontendEditingTrigger::class, 'onUpdate'];
 $GLOBALS['TL_HOOKS']['catalogManagerEntityOnDelete'][] = [FrontendEditingTrigger::class, 'onDelete'];
-
-$GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE']['CATALOG_MAILER'] = [
-    'DEFAULT_MAILER' => [
-        'recipients' => ['admin_email', 'recipient', 'raw_*', 'clean_*', 'post_raw_*', 'post_clean_*', 'post_old_raw_*', 'post_old_clean_*', 'post_type'],
-        'attachment_tokens' => ['raw_*', 'clean_*', 'field_*', 'table_*', 'post_raw_*', 'post_clean_*', 'post_old_raw_*', 'post_old_clean_*', 'post_type'],
-        'email_replyTo' => ['admin_email', 'recipient', 'raw_*', 'clean_*', 'post_raw_*', 'post_clean_*', 'post_old_raw_*', 'post_old_clean_*', 'post_type'],
-        'email_sender_name' => ['admin_email', 'recipient', 'raw_*', 'clean_*', 'post_raw_*', 'post_clean_*', 'post_old_raw_*', 'post_old_clean_*', 'post_type'],
-        'email_recipient_cc' => ['admin_email', 'recipient', 'raw_*', 'clean_*', 'post_raw_*', 'post_clean_*', 'post_old_raw_*', 'post_old_clean_*', 'post_type'],
-        'file_name' => ['admin_email', 'raw_*', 'clean_*', 'field_*', 'table_*', 'post_raw_*', 'post_clean_*', 'post_old_raw_*', 'post_old_clean_*', 'post_type'],
-        'email_recipient_bcc' => ['admin_email', 'recipient', 'raw_*', 'clean_*', 'post_raw_*', 'post_clean_*', 'post_old_raw_*', 'post_old_clean_*', 'post_type'],
-        'email_sender_address' => ['admin_email', 'recipient', 'raw_*', 'clean_*', 'post_raw_*', 'post_clean_*', 'post_old_raw_*', 'post_old_clean_*', 'post_type'],
-        'file_content' => ['admin_email', 'raw_*', 'clean_*', 'field_*', 'table_*', 'post_raw_*', 'post_clean_*', 'post_old_raw_*', 'post_old_clean_*', 'post_type', 'reminder_attachment'],
-        'email_subject' => ['admin_email', 'domain', 'recipient', 'raw_*', 'clean_*', 'post_raw_*', 'post_clean_*', 'post_old_raw_*', 'post_old_clean_*', 'post_type'],
-        'email_text' => ['admin_email', 'recipient', 'raw_*', 'clean_*', 'field_*', 'table_*', 'post_raw_*', 'post_clean_*', 'post_old_raw_*', 'post_old_clean_*', 'post_type', 'reminder_attachment'],
-        'email_html' => ['admin_email', 'recipient', 'raw_*', 'clean_*', 'field_*', 'table_*', 'post_raw_*', 'post_clean_*', 'post_old_raw_*', 'post_old_clean_*', 'post_type', 'reminder_attachment']
-    ]
-];
